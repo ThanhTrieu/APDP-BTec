aef96d0375b0c0b1db2b02246d0c6155db5e1f3c
unknown <nguyenthanhtrieu90@gmail.com>
2024-08-07 15:42:04 +0700

    update code

---
+++ SIMS_SE06205/Controllers/CourseController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SIMS_SE06205.Controllers
+{
+    public class CourseController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ SIMS_SE06205/Controllers/LoginController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SIMS_SE06205.Models;
+
+namespace SIMS_SE06205.Controllers
+{
+    public class LoginController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            LoginViewModel vm = new LoginViewModel();
+            return View(vm);
+        }
+
+        [HttpPost]
+        public IActionResult Index(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                string username = model.UserName.Trim();
+                string password = model.Password.Trim();
+                string dataJson = System.IO.File.ReadAllText(@"C:\\CSharp\\data-sims\\data-user.json");
+           
+                // kiem tra username va password co ton tai trong dataJson hay khong ?
+
+                return Ok(dataJson);
+            }
+            return View(model);
+        }
+    }
+}
